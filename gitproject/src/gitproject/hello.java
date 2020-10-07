0eb434006b3fb4a422514f581572fe615bdf03d4
yj-603-13 <yj-603-13@DESKTOP-QHSA7A1>
2020-10-07 10:48:46 +0900

    message1

--- gitproject/src/gitproject/hello.java
+++ gitproject/src/gitproject/hello.java
@@ -4,6 +4,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		System.out.print("hello world!!");
+		System.out.print("hello world!!");
+		System.out.print("hello world!!");
+		
 	}
 
 }
